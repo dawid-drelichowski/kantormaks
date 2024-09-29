aa22948b39a5564f583dc4bfeccaed9c9d71813e
Dawid <dawid.drelichowski@gmail.com>
2024-09-29 20:06:39 +0200

    Database dumb data cleanup, edition tests addition

--- packages/website/tests/admin.spec.js
+++ packages/website/tests/admin.spec.js
@@ -10,7 +10,8 @@ test.describe('Admin page', () => {
     expect(response.status()).toBe(401)
   })
 
-  test('displays properly', async ({ browser }) => {
+  test('save changes properly', async ({ browser }) => {
+    const valueToSet = '1.1'
     const context = await browser.newContext({
       httpCredentials: {
         username: config.WEBSITE_ADMIN_USER,
@@ -21,6 +22,23 @@ test.describe('Admin page', () => {
     await page.goto(adminUrl)
 
     await expect(page).toHaveTitle('Kantor Maks Wrocław - Panel administratora')
+    let inputs = page.getByRole('spinbutton')
+
+    for (const input of await inputs.all()) {
+      await input.fill(valueToSet)
+    }
+
+    await Promise.all([
+      page.getByRole('button').click(),
+      page.waitForURL(`${adminUrl}?success=true`),
+    ]);
+
+    await expect(page.getByText('Zmiany zostały zapisane')).toBeVisible()
+    inputs = page.getByRole('spinbutton')
+
+    for (const input of await inputs.all()) {
+      expect(await input.inputValue()).toBe(valueToSet)
+    }
 
     await browser.close()
   })
